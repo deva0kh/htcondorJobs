cf9eed4501377278db3eae21435e066ae9387b7c
deva0kh <achraf.kh03@gmail.com>
2021-01-29 00:34:52 +0000

    pi fixe vanilla

--- job_vanilla/job2/pi.cpp
+++ job_vanilla/job2/pi.cpp
@@ -1,20 +1,24 @@
-
-
-#include "cmath" 
-#include "iostream" 
-using namespace std; 
-  
-
-void printValueOfPi() 
-{ 
-    // Print value of pi 
-    printf("%f\n", M_PI); 
-} 
-  
-
-int main() 
-{ 
-
-    printValueOfPi(); 
-    return 0; 
-} 
+#include<iostream>
+using namespace std;
+int main()
+{
+int c=0;
+double pi=0;
+float a=200000;
+for(a=1;a<=200000;a=a+2)
+    {
+        if(c==0)
+        {
+            pi=pi+4/a;
+            c++;
+        }
+        else
+        {
+            pi=pi-4/a;
+            c--;
+        }
+    }
+    cout<<"PI's Value = "<<pi<<endl;
+return 0;
+}
+ 
