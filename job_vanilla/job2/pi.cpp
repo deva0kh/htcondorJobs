f84cdcb8894a6ecc1e1f83af8058b3e255433427
deva0kh <achraf.kh03@gmail.com>
2021-01-29 00:31:17 +0000

    pi job

---
+++ job_vanilla/job2/pi.cpp
@@ -0,0 +1,20 @@
+
+
+#include "cmath" 
+#include "iostream" 
+using namespace std; 
+  
+
+void printValueOfPi() 
+{ 
+    // Print value of pi 
+    printf("%f\n", M_PI); 
+} 
+  
+
+int main() 
+{ 
+
+    printValueOfPi(); 
+    return 0; 
+} 
